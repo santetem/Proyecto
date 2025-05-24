dddbe5847209d3f168416765e22a2e849247df8c
santetem <santiago.diazet32@gmail.com>
2025-05-24 19:38:44 -0300

    Add files via upload

---
+++ Santiago Díaz 1 B/Código 1 B.ino
@@ -0,0 +1,76 @@
+#define papu1 2
+#define papu2 3
+#define papu3 4
+#define papu4 5
+#define papu5 6
+#define papu6 7
+#define papu7 8
+#define papu8 9
+#define papu9 10
+#define papu10 11
+
+void setup()
+{
+  pinMode(papu1 , OUTPUT);
+  pinMode(papu2 , OUTPUT);
+  pinMode(papu3 , OUTPUT);
+  pinMode(papu4 , OUTPUT);
+  pinMode(papu5 , OUTPUT);
+  pinMode(papu6 , OUTPUT);
+  pinMode(papu7 , OUTPUT);
+  pinMode(papu8 , OUTPUT);
+  pinMode(papu9 , OUTPUT);
+  pinMode(papu10 , OUTPUT);
+}
+void loop()
+{
+  digitalWrite(papu1 , HIGH);
+  delay(1000);
+  digitalWrite(papu1 , LOW);
+  delay(1000);
+  
+  digitalWrite(papu2 , HIGH);
+  delay(1000);
+  digitalWrite(papu2 , LOW);
+  delay(1000);
+  
+  digitalWrite(papu3 , HIGH);
+  delay(1000);
+  digitalWrite(papu3 , LOW);
+  delay(1000);
+  
+  digitalWrite(papu4 , HIGH);
+  delay(1000);
+  digitalWrite(papu4 , LOW);
+  delay(1000);
+  
+  digitalWrite(papu5 , HIGH);
+  delay(1000);
+  digitalWrite(papu5 , LOW);
+  delay(1000);
+  
+  digitalWrite(papu6 , HIGH);
+  delay(1000);
+  digitalWrite(papu6 , LOW);
+  delay(1000);
+  
+  digitalWrite(papu7 , HIGH);
+  delay(1000);
+  digitalWrite(papu7 , LOW);
+  delay(1000);
+  
+  digitalWrite(papu8 , HIGH);
+  delay(1000);
+  digitalWrite(papu8 , LOW);
+  delay(1000);
+  
+  digitalWrite(papu9 , HIGH);
+  delay(1000);
+  digitalWrite(papu9 , LOW);
+  delay(1000);
+  
+  digitalWrite(papu10 , HIGH);
+  delay(1000);
+  digitalWrite(papu10 , LOW);
+  delay(1000);
+}
