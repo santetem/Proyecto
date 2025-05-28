2aadbfa9a6cf6dce448ce970c616849ffe814eff
santetem <santiago.diazet32@gmail.com>
2025-05-28 11:14:58 -0300

    Add files via upload

---
+++ Santiago Díaz 1 G/Código 1 G.ino
@@ -0,0 +1,42 @@
+const int boton = 5;
+const int verde = 2;
+const int azul = 3;
+const int rojo = 4;
+int cont = 0;
+
+void setup()
+{
+  pinMode(rojo, OUTPUT);
+  pinMode(azul, OUTPUT);
+  pinMode(verde, OUTPUT);
+  pinMode(boton, INPUT_PULLUP);
+}
+
+void loop()
+{
+  if (digitalRead(boton) == LOW) {
+    cont = cont + 1;
+    if (cont >= 3) {
+      cont = 0;
+    }
+    delay(200);
+  }
+
+  if (cont == 0) {
+    digitalWrite(rojo, HIGH);
+    digitalWrite(azul, LOW);
+    digitalWrite(verde, LOW);
+  }
+
+  if (cont == 1) {
+    digitalWrite(rojo, LOW);
+    digitalWrite(azul, HIGH);
+    digitalWrite(verde, LOW);
+  }
+
+  if (cont == 2) {
+    digitalWrite(rojo, LOW);
+    digitalWrite(azul, LOW);
+    digitalWrite(verde, HIGH);
+  }
+}
